dd43a47d000c8938b8834a6a2e04849a90bbf22c
Peter Astl <astlpeter@gmail.com>
2025-11-16 18:09:07 +0100

    Day 34 Quizz with API

---
+++ 35/main.py
@@ -0,0 +1,25 @@
+import requests
+
+from info import API_KEY
+
+weather = "https://api.openweathermap.org/data/2.5/forecast"
+
+weather_params = {
+    "appid": API_KEY,
+    "lat" : 48.172375,
+    "lon" : 16.286915,
+    "cnt" : 4
+
+}
+
+response = requests.get(weather,params=weather_params)
+
+x = response.json()
+will_rain = False
+for hour in x["list"]:
+    condition_code = hour["weather"][0]["id"]
+    if int(condition_code) >= 700:
+        will_rain = True
+
+if will_rain:
+    print("Raining")
