62540625b53cf184bf2ba3a415a666e996d88d4e
Peter Astl <astlpeter@gmail.com>
2025-12-06 21:53:40 +0100

    Day 55 Commit First Half

--- 55/main.py
+++ 55/main.py
@@ -2,10 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
+@make_bold
+@make_emphasis
+@make_underlined
 def hello_world():
-    return 'Hello, World!'
+    return ('<h1 style = "text-align: cemter"> Hello, World!</h1>'
+            '<p>This is an example HTML page</p>'
+            '<img src = "https://gifuu.agency/wp-content/uploads/2020/12/GIFuu-Agentur-GIF-Marketing-Maskottchen-Olaf.gif" width=400>'
+            '<u><em><b>Bye</b></em></u>')
+
+@app.route("/username/<name>")
+def hello_user(name):
+    return f"Hello, {name}!"
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
