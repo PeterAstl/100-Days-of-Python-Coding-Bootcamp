9c8cc822d5d67f5b11c96f8e3d04f8cf3ba9bfa9
Peter Astl <astlpeter@gmail.com>
2025-12-14 15:41:58 +0100

    Day 58 Bootstrap finished

--- 58 Web Foundation Bootstrap/main.py
+++ 58 Web Foundation Bootstrap/main.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 FLASK_APP = Flask(__name__)
 
 @FLASK_APP.route('/')
 def home():
-    return '<h1>Hello World!</h1>'
+    return render_template("website.html")
+
+if __name__ == '__main__':
+    FLASK_APP.run()
